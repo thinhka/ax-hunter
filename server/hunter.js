b36a14279e2c0c76f259b29dc9a559c10797d575
Thinh Do <thinh.do@soldenservices.com>
2021-11-28 00:12:40 +0700

    Update hunter.js

--- server/hunter.js
+++ server/hunter.js
@@ -22,20 +22,22 @@ module.exports = {
   async run() {
     var that = this;
     new CronJob('*/5 * * * * *', async function() {
-      that.fetchAqua();
-      that.fetchBird();
+      var aquaRequest = {"operationName":"GetAxieBriefList","variables":{"from":0,"size":10,"sort":"PriceAsc","auctionType":"Sale","owner":null,"criteria":{"region":null,"parts":["eyes-mavis","ears-nimo","tail-nimo","back-anemone","horn-anemone","mouth-lam","ears-inkling","eyes-gero"],"bodyShapes":null,"classes":["Aquatic"],"stages":null,"numMystic":null,"pureness":null,"title":null,"breedable":null,"breedCount":null,"hp":[],"skill":[],"speed":[],"morale":[]},"filterStuckAuctions":true},"query":"query GetAxieBriefList($auctionType: AuctionType, $criteria: AxieSearchCriteria, $from: Int, $sort: SortBy, $size: Int, $owner: String, $filterStuckAuctions: Boolean) {\n  axies(\n    auctionType: $auctionType\n    criteria: $criteria\n    from: $from\n    sort: $sort\n    size: $size\n    owner: $owner\n    filterStuckAuctions: $filterStuckAuctions\n  ) {\n    total\n    results {\n      ...AxieBrief\n      __typename\n    }\n    __typename\n  }\n}\n\nfragment AxieBrief on Axie {\n  id\n  name\n  stage\n  class\n  breedCount\n  image\n  title\n  battleInfo {\n    banned\n    __typename\n  }\n  auction {\n    currentPrice\n    currentPriceUSD\n    __typename\n  }\n  parts {\n    id\n    name\n    class\n    type\n    specialGenes\n    __typename\n  }\n  __typename\n}\n"};
+      var birdRequest = {"operationName":"GetAxieBriefList","variables":{"from":0,"size":10,"sort":"PriceAsc","auctionType":"Sale","owner":null,"criteria":{"region":null,"parts":["eyes-mavis","ears-owl","back-pigeon-post","mouth-little-owl","horn-eggshell","tail-post-fight"],"bodyShapes":null,"classes":["Bird"],"stages":null,"numMystic":null,"pureness":null,"title":null,"breedable":null,"breedCount":null,"hp":[],"skill":[],"speed":[],"morale":[]},"filterStuckAuctions":true},"query":"query GetAxieBriefList($auctionType: AuctionType, $criteria: AxieSearchCriteria, $from: Int, $sort: SortBy, $size: Int, $owner: String, $filterStuckAuctions: Boolean) {\n  axies(\n    auctionType: $auctionType\n    criteria: $criteria\n    from: $from\n    sort: $sort\n    size: $size\n    owner: $owner\n    filterStuckAuctions: $filterStuckAuctions\n  ) {\n    total\n    results {\n      ...AxieBrief\n      __typename\n    }\n    __typename\n  }\n}\n\nfragment AxieBrief on Axie {\n  id\n  name\n  stage\n  class\n  breedCount\n  image\n  title\n  battleInfo {\n    banned\n    __typename\n  }\n  auction {\n    currentPrice\n    currentPriceUSD\n    __typename\n  }\n  parts {\n    id\n    name\n    class\n    type\n    specialGenes\n    __typename\n  }\n  __typename\n}\n"};
+      that.fetch(aquaRequest, 250);
+      that.fetch(birdRequest, 312);
     }, null, true).start();
   },
 
-  async fetchAqua() {  
+  async fetch(requestData, lowPrice) {  
     var that = this;
     var request = require('request');
-    var myJSONObject = {"operationName":"GetAxieBriefList","variables":{"from":0,"size":10,"sort":"PriceAsc","auctionType":"Sale","owner":null,"criteria":{"region":null,"parts":["eyes-mavis","ears-nimo","tail-nimo","back-anemone","horn-anemone","mouth-lam","ears-inkling","eyes-gero"],"bodyShapes":null,"classes":["Aquatic"],"stages":null,"numMystic":null,"pureness":null,"title":null,"breedable":null,"breedCount":null,"hp":[],"skill":[],"speed":[],"morale":[]},"filterStuckAuctions":true},"query":"query GetAxieBriefList($auctionType: AuctionType, $criteria: AxieSearchCriteria, $from: Int, $sort: SortBy, $size: Int, $owner: String, $filterStuckAuctions: Boolean) {\n  axies(\n    auctionType: $auctionType\n    criteria: $criteria\n    from: $from\n    sort: $sort\n    size: $size\n    owner: $owner\n    filterStuckAuctions: $filterStuckAuctions\n  ) {\n    total\n    results {\n      ...AxieBrief\n      __typename\n    }\n    __typename\n  }\n}\n\nfragment AxieBrief on Axie {\n  id\n  name\n  stage\n  class\n  breedCount\n  image\n  title\n  battleInfo {\n    banned\n    __typename\n  }\n  auction {\n    currentPrice\n    currentPriceUSD\n    __typename\n  }\n  parts {\n    id\n    name\n    class\n    type\n    specialGenes\n    __typename\n  }\n  __typename\n}\n"}
+    
     request({
         url: "https://graphql-gateway.axieinfinity.com/graphql",
         method: "POST",
         json: true,
-        body: myJSONObject
+        body: requestData
     }, function (error, response, body){
       try{
         var results = body.data.axies.results;
@@ -49,7 +51,7 @@ module.exports = {
           if(!pusheds.includes(tokenId)){
             if(pusheds.length > 15){ pusheds = [];}            
 
-            if(p.auction.currentPriceUSD <= 250){
+            if(p.auction.currentPriceUSD <= lowPrice){
               that.postTeleMessage(axieName, breedCount, currentPrice, currentPriceUSD, tokenId);
               pusheds.push(tokenId);
             }
@@ -58,40 +60,6 @@ module.exports = {
       } catch (error) {
         console.error(error);
       }    
-    });
-  },
-
-  async fetchBird() {  
-    var that = this;
-    var request = require('request');
-    var myJSONObject = {"operationName":"GetAxieBriefList","variables":{"from":0,"size":10,"sort":"PriceAsc","auctionType":"Sale","owner":null,"criteria":{"region":null,"parts":["eyes-mavis","ears-owl","back-pigeon-post","mouth-little-owl","horn-eggshell","tail-post-fight"],"bodyShapes":null,"classes":["Bird"],"stages":null,"numMystic":null,"pureness":null,"title":null,"breedable":null,"breedCount":null,"hp":[],"skill":[],"speed":[],"morale":[]},"filterStuckAuctions":true},"query":"query GetAxieBriefList($auctionType: AuctionType, $criteria: AxieSearchCriteria, $from: Int, $sort: SortBy, $size: Int, $owner: String, $filterStuckAuctions: Boolean) {\n  axies(\n    auctionType: $auctionType\n    criteria: $criteria\n    from: $from\n    sort: $sort\n    size: $size\n    owner: $owner\n    filterStuckAuctions: $filterStuckAuctions\n  ) {\n    total\n    results {\n      ...AxieBrief\n      __typename\n    }\n    __typename\n  }\n}\n\nfragment AxieBrief on Axie {\n  id\n  name\n  stage\n  class\n  breedCount\n  image\n  title\n  battleInfo {\n    banned\n    __typename\n  }\n  auction {\n    currentPrice\n    currentPriceUSD\n    __typename\n  }\n  parts {\n    id\n    name\n    class\n    type\n    specialGenes\n    __typename\n  }\n  __typename\n}\n"};
-    request({
-        url: "https://graphql-gateway.axieinfinity.com/graphql",
-        method: "POST",
-        json: true,
-        body: myJSONObject
-    }, function (error, response, body){
-      try {
-          var results = body.data.axies.results;
-          results.forEach(p => {
-            var axieName = p.class;
-            var breedCount = p.breedCount;
-            var tokenId = p.id;
-            var currentPrice = p.auction.currentPrice.substring(0,3);;
-            var currentPriceUSD = p.auction.currentPriceUSD;
-
-            if(!pusheds.includes(tokenId)){
-              if(pusheds.length > 15){ pusheds = [];}            
-
-              if(p.auction.currentPriceUSD <= 315){
-                that.postTeleMessage(axieName, breedCount, currentPrice, currentPriceUSD, tokenId);
-                pusheds.push(tokenId);
-              }
-            }
-          });   
-        } catch (error) {
-          console.error(error);
-        }   
     });
   },
 
