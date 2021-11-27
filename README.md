cda47789b148de8bbb97caae239577ff054af3f7
Thinh Do <thinh.do@soldenservices.com>
2021-11-28 00:01:33 +0700

    init

---
+++ README.md
@@ -0,0 +1,33 @@
+# alert_pool_simba
+
+Channel running it: https://t.me/simbaalertpool
+
+How to start:
+
+1.  Install nodejs
+2.  Open terminal and ```cd {your winning_reward_simba folder}```
+3.  ```npm install```
+4.  ```npm start```
+
+```
+======= Claim SIM NOW ======= 
+Chain: Polygon (Matic) 
+Pool balance: 0 
+Latest update: 05:21:30 (UTC) 
+=============================
+```
+
+
+```
+======= Claim SIM NOW ======= 
+Chain: Bsc (Binance smart chain) 
+Pool balance: 0 
+Latest update: 05:21:30 (UTC) 
+=============================
+```
+
+So ez game!!
+
+```
+Donate: 0x843C9034e9cEcCEc85ceCC1B87dB3e260e9ea5eE (MATIC)
+```
---
+++ server/hunter.js
@@ -0,0 +1,110 @@
+const Web3 = require('web3');
+var moment = require('moment');
+var CronJob = require('cron').CronJob;
+const TelegramBot = require('node-telegram-bot-api');
+const { json } = require('body-parser');
+const { type } = require('os');
+const token = '2125295221:AAHsHhtG942Mcr6kI4FABVVLvuN_yvsV9eo';
+const bot = new TelegramBot(token, {
+  polling: true
+});
+
+const channelChatId = '2045766841';
+
+var pusheds = [];
+var detailMetamaskUrl = 'https://marketplace.axieinfinity.com/axie/';
+var detailWebUrl = 'https://itam.games/markets/lime-odyssey/';
+
+
+
+
+module.exports = {
+  async run() {
+    var that = this;
+    new CronJob('*/5 * * * * *', async function() {
+      that.fetchAqua();
+      that.fetchBird();
+    }, null, true).start();
+  },
+
+  async fetchAqua() {  
+    var that = this;
+    var request = require('request');
+    var myJSONObject = {"operationName":"GetAxieBriefList","variables":{"from":0,"size":10,"sort":"PriceAsc","auctionType":"Sale","owner":null,"criteria":{"region":null,"parts":["eyes-mavis","ears-nimo","tail-nimo","back-anemone","horn-anemone","mouth-lam","ears-inkling","eyes-gero"],"bodyShapes":null,"classes":["Aquatic"],"stages":null,"numMystic":null,"pureness":null,"title":null,"breedable":null,"breedCount":null,"hp":[],"skill":[],"speed":[],"morale":[]},"filterStuckAuctions":true},"query":"query GetAxieBriefList($auctionType: AuctionType, $criteria: AxieSearchCriteria, $from: Int, $sort: SortBy, $size: Int, $owner: String, $filterStuckAuctions: Boolean) {\n  axies(\n    auctionType: $auctionType\n    criteria: $criteria\n    from: $from\n    sort: $sort\n    size: $size\n    owner: $owner\n    filterStuckAuctions: $filterStuckAuctions\n  ) {\n    total\n    results {\n      ...AxieBrief\n      __typename\n    }\n    __typename\n  }\n}\n\nfragment AxieBrief on Axie {\n  id\n  name\n  stage\n  class\n  breedCount\n  image\n  title\n  battleInfo {\n    banned\n    __typename\n  }\n  auction {\n    currentPrice\n    currentPriceUSD\n    __typename\n  }\n  parts {\n    id\n    name\n    class\n    type\n    specialGenes\n    __typename\n  }\n  __typename\n}\n"}
+    request({
+        url: "https://graphql-gateway.axieinfinity.com/graphql",
+        method: "POST",
+        json: true,
+        body: myJSONObject
+    }, function (error, response, body){
+      try{
+        var results = body.data.axies.results;
+        results.forEach(p => {
+          var axieName = p.class;
+          var breedCount = p.breedCount;
+          var tokenId = p.id;
+          var currentPrice = p.auction.currentPrice.substring(0,3);;
+          var currentPriceUSD = p.auction.currentPriceUSD;
+
+          if(!pusheds.includes(tokenId)){
+            if(pusheds.length > 15){ pusheds = [];}            
+
+            if(p.auction.currentPriceUSD <= 250){
+              that.postTeleMessage(axieName, breedCount, currentPrice, currentPriceUSD, tokenId);
+              pusheds.push(tokenId);
+            }
+          }
+        });
+      } catch (error) {
+        console.error(error);
+      }    
+    });
+  },
+
+  async fetchBird() {  
+    var that = this;
+    var request = require('request');
+    var myJSONObject = {"operationName":"GetAxieBriefList","variables":{"from":0,"size":10,"sort":"PriceAsc","auctionType":"Sale","owner":null,"criteria":{"region":null,"parts":["eyes-mavis","ears-owl","back-pigeon-post","mouth-little-owl","horn-eggshell","tail-post-fight"],"bodyShapes":null,"classes":["Bird"],"stages":null,"numMystic":null,"pureness":null,"title":null,"breedable":null,"breedCount":null,"hp":[],"skill":[],"speed":[],"morale":[]},"filterStuckAuctions":true},"query":"query GetAxieBriefList($auctionType: AuctionType, $criteria: AxieSearchCriteria, $from: Int, $sort: SortBy, $size: Int, $owner: String, $filterStuckAuctions: Boolean) {\n  axies(\n    auctionType: $auctionType\n    criteria: $criteria\n    from: $from\n    sort: $sort\n    size: $size\n    owner: $owner\n    filterStuckAuctions: $filterStuckAuctions\n  ) {\n    total\n    results {\n      ...AxieBrief\n      __typename\n    }\n    __typename\n  }\n}\n\nfragment AxieBrief on Axie {\n  id\n  name\n  stage\n  class\n  breedCount\n  image\n  title\n  battleInfo {\n    banned\n    __typename\n  }\n  auction {\n    currentPrice\n    currentPriceUSD\n    __typename\n  }\n  parts {\n    id\n    name\n    class\n    type\n    specialGenes\n    __typename\n  }\n  __typename\n}\n"};
+    request({
+        url: "https://graphql-gateway.axieinfinity.com/graphql",
+        method: "POST",
+        json: true,
+        body: myJSONObject
+    }, function (error, response, body){
+      try {
+          var results = body.data.axies.results;
+          results.forEach(p => {
+            var axieName = p.class;
+            var breedCount = p.breedCount;
+            var tokenId = p.id;
+            var currentPrice = p.auction.currentPrice.substring(0,3);;
+            var currentPriceUSD = p.auction.currentPriceUSD;
+
+            if(!pusheds.includes(tokenId)){
+              if(pusheds.length > 15){ pusheds = [];}            
+
+              if(p.auction.currentPriceUSD <= 315){
+                that.postTeleMessage(axieName, breedCount, currentPrice, currentPriceUSD, tokenId);
+                pusheds.push(tokenId);
+              }
+            }
+          });   
+        } catch (error) {
+          console.error(error);
+        }   
+    });
+  },
+
+  postTeleMessage(nftTypeName, breedCount, price, priceUsd, tokenId) {
+    price = price/10000;
+    var str_balance = `<b>${nftTypeName}</b> - Breed count: ${breedCount}\n`;
+    str_balance += `Price: ${price} -  $${priceUsd}\n`;
+    str_balance += `<a>${detailMetamaskUrl}${tokenId}</a> \n`;          
+    bot.sendMessage(channelChatId, str_balance, {
+      disable_web_page_preview: true,
+      disable_notification: false,
+      parse_mode: "HTML"
+    });
+  },
+
+}
